70dd55e9e3121c8590413bd4fb73dfa01b93c8d5
o0oo0ooo0 <42055259+o0oo0ooo0@users.noreply.github.com>
2020-12-14 19:20:46 +0800

    支持多账号

--- main.py
+++ main.py
@@ -128,8 +128,8 @@ def main_handler(event, context):
     if not USERNAME or not PASSWORD:
         print_("你没有添加任何账户")
         exit(1)
-    user_list = USERNAME.split(',')
-    passwd_list = PASSWORD.split(',')
+    user_list = USERNAME.strip().split()
+    passwd_list = PASSWORD.strip().split()
     if len(user_list) != len(passwd_list):
         print_("The number of usernames and passwords do not match!")
         exit(1)
